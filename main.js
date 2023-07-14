a88e2d1d81e204b59163942aa7aee67ffb9c6967
GabRioBlu <67011825+GabRioBlu@users.noreply.github.com>
2023-07-13 20:58:19 -0400

    how do i do this

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log('Hello World!');
+const canvas = document.getElementById("Display");
+const ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(20, 20, 100, 100);
+ctx.stroke();
